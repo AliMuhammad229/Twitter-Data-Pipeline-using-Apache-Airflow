3f199c9b97307f0c33c31a2a40521452e90aa9ea
Ali Muhammad <47716529+AliMuhammad229@users.noreply.github.com>
2024-03-30 19:37:05 +0500

    Update twitter_dags.py

--- twitter_dags.py
+++ twitter_dags.py
@@ -8,7 +8,7 @@ from etl_twitter import run_twitter_etl
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime(2020, 11, 8),
+    'start_date': datetime(2024, 3, 20),
     'email': ['airflow@example.com'],
     'email_on_failure': False,
     'email_on_retry': False,
